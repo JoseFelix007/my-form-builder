dfd6e38deecda5931ec27bed5f7f4a692f5f9bfd
Jose Felix <jafelixramos@gmail.com>
2024-05-29 15:36:13 -0500

    [feature][style] add scrollbar

--- src/components/Form/FormQuestion/FormQuestionList.jsx
+++ src/components/Form/FormQuestion/FormQuestionList.jsx
@@ -2,7 +2,7 @@ import FormQuestionDraggable from './FormQuestionDraggable';
 
 const FormQuestionList = ({ innerRef, formQuestions, onDeleteQuestion, placeholder, ...droppableProps }) => {
   return (
-    <div className='form | relative flex-col gap-1 w-full' ref={innerRef} {...droppableProps}>
+    <div className='form | relative flex-col gap-1 w-full max-h-70 overflow-y-scroll scrollbar' ref={innerRef} {...droppableProps}>
       {
         formQuestions.length > 0 &&
         formQuestions.map((formQuestion, index) => (
--- src/components/Utils/Card.jsx
+++ src/components/Utils/Card.jsx
@@ -1,16 +1,14 @@
 import './Card.scss';
 
-const Card = ({ title, children }) => {
+const Card = ({ title, children, className }) => {
   return (
-    <>
-      <div className="c-card">
-        <div className="c-card__title">{ title }</div>
-        { title && <hr className="c-card__sep" /> }
-        <div className="c-card__body">
-            { children }
-        </div>
+    <div className={`c-card ${className}`}>
+      <div className="c-card__title">{ title }</div>
+      { title && <hr className="c-card__sep" /> }
+      <div className="c-card__body">
+          { children }
       </div>
-    </>
+    </div>
   );
 }
 
